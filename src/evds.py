c4cc400a1ea8a1816bbe57e3cc90a22a0c82f473
umutGulsen <umutjean@gmail.com>
2024-07-15 13:36:10 +0300

    "first files created"

---
+++ src/evds.py
@@ -0,0 +1,8 @@
+def test():
+    print("TEST")
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ src/finext.py
@@ -0,0 +1,9 @@
+import evds
+
+
+def main():
+    evds.test()
+
+
+if __name__ == '__main__':
+    main()
