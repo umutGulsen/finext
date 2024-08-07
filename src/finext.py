f58bbbd2d5c412db79ba6a7bbce51dac2823cd32
umutGulsen <umutjean@gmail.com>
2024-08-07 12:26:32 +0300

    added a mode selection menu at the start

--- src/finext.py
+++ src/finext.py
@@ -2,11 +2,18 @@ import evds_reader
 import evds_data_tour
 
 def main():
-    #tour = evds_data_tour.EvdsExplorer()
-    #tour.get_main_categories()
-
-    df = evds_reader.read_evds_data("TP.FG.J0")
-    evds_reader.write_evds_Data(df, "cpi.csv")
+    while True:
+        mode = input("Choose mode (Data Tour: t | Export Data: e): ")
+        if mode == "t":
+            tour = evds_data_tour.EvdsExplorer()
+            tour.get_main_categories()
+            break
+        elif mode == "e":
+            df = evds_reader.read_evds_data("TP.FG.J0")
+            evds_reader.write_evds_data(df, "cpi.csv")
+            break
+        else:
+            print("Please choose a valid mode")
 
 if __name__ == '__main__':
     main()
